c181bad6cfa35b903bd6b4975d679a325b56d208
-akku- <akkun11.open@gmail.com>
2025-04-09 19:58:08 +0900

    first gtk4 app

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from fastvideocreator!")
+import gi
+gi.require_version("GLib", "2.0")
+gi.require_version("Gtk", "4.0")
 
+import sys
+from gi.repository import GLib, Gtk
+
+class MyApplication(Gtk.Application):
+  def __init__(self):
+    super().__init__(application_id="io.github.akku1139.FastVideoCreator")
+    GLib.set_application_name("FastVideoCreator")
+    self.connect("activate", self.on_activate)
+
+  def on_activate(self, *_args):
+    window = Gtk.ApplicationWindow(application=self, title="FastVideoCreator")
+    window.present()
+
+def main():
+  app = MyApplication()
+  exit_status = app.run(sys.argv)
+  sys.exit(exit_status)
 
 if __name__ == "__main__":
-    main()
+  main()
